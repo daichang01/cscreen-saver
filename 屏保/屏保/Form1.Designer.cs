bb9d8b82881b0b38186419834016c279597e0963
DC <18181985920@163.com>
2020-03-28 23:32:32 +0800

    Signed-off-by: DC <18181985920@163.com>

---
+++ 屏保/屏保/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace 屏保
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_win = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_win
+            // 
+            this.label_win.AutoSize = true;
+            this.label_win.Font = new System.Drawing.Font("宋体", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_win.ForeColor = System.Drawing.Color.White;
+            this.label_win.Location = new System.Drawing.Point(218, 165);
+            this.label_win.Name = "label_win";
+            this.label_win.Size = new System.Drawing.Size(522, 97);
+            this.label_win.TabIndex = 0;
+            this.label_win.Text = "Windows 10";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Font = new System.Drawing.Font("宋体", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_time.ForeColor = System.Drawing.Color.White;
+            this.label_time.Location = new System.Drawing.Point(196, 394);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(220, 64);
+            this.label_time.TabIndex = 1;
+            this.label_time.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(753, 635);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.label_win);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_win;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_time;
+    }
+}
+
---
+++ 屏保/屏保/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 屏保
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int deltx = 5;
+        int delty = 4;
+        int timex = 4;
+        int timey = 5;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            this.label_win.Left += deltx;
+            this.label_win.Top += delty;
+            this.label_win.ForeColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            if (this.label_win.Top < 0 ||
+                this.label_win.Top + this.label_win.Height > this.Height)
+                delty = -delty;
+            if (this.label_win.Left < 0 ||
+                this.label_win.Left + this.label_win.Width > this.Width)
+                deltx = -deltx;
+            this.label_time.Text = DateTime.Now.ToString();
+            //this.label_time.ForeColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            this.label_time.Left -= timex;
+            this.label_time.Top -= timey;
+            if (this.label_time.Top < 0 ||
+                this.label_time.Top + this.label_time.Height > this.Height)
+                timey = -timey;
+            if (this.label_time.Left < 0 ||
+                this.label_time.Left + this.label_time.Width > this.Width)
+                timex = -timex;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Exit(); 
+        }
+    }
+}
